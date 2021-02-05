4172a84659f5903d8810fee587cb24dcb66b8327
Hagishilta <johnl@avokadolab.com>
2021-02-05 17:35:27 +0900

    Update : added chinese characters

--- RaspberryPi/c/examples/LCD_2in4_test.c
+++ RaspberryPi/c/examples/LCD_2in4_test.c
@@ -59,19 +59,19 @@ void LCD_2IN4_test(void)
 
     Paint_DrawNum(5, 120, 123456789, &Font20, GREEN, IMAGE_BACKGROUND);
     Paint_DrawString_CN(5,160, "微雪电子",  &Font24CN,IMAGE_BACKGROUND,BLUE);
-    Paint_DrawString_CN(5,200, "风风风风", &Font12CN, IMAGE_BACKGROUND, BLUE);
+    Paint_DrawString_CN(5,200, "风风风风", &Font24CN, IMAGE_BACKGROUND, BLUE);
     // Paint_DrawString_EN(5,260, "hihi", &Font24CN, IMAGE_BACKGROUND, BLUE); 
-    Paint_DrawString_CN(5,240, "米饭米饭", &Font12CN, IMAGE_BACKGROUND, BLUE);
-    Paint_DrawString_CN(5,300, "微雪电子",  &Font12CN,IMAGE_BACKGROUND,BLUE);
+    Paint_DrawString_CN(5,240, "米饭米饭", &Font24CN, IMAGE_BACKGROUND, BLUE);
+    Paint_DrawString_CN(5,300, "微雪电子",  &Font24CN,IMAGE_BACKGROUND,BLUE);
     // /*3.Refresh the picture in RAM to LCD*/
     LCD_2IN4_Display((UBYTE *)BlackImage);
 	DEV_Delay_ms(3000);
     // /* show bmp */
 	printf("show bmp\r\n");
 	
-	GUI_ReadBmp("./pic/LCD_2inch.bmp");    
-    LCD_2IN4_Display((UBYTE *)BlackImage);
-    DEV_Delay_ms(3000);
+	// GUI_ReadBmp("./pic/LCD_2inch.bmp");    
+    // LCD_2IN4_Display((UBYTE *)BlackImage);
+    // DEV_Delay_ms(3000);
 	
 
 	
